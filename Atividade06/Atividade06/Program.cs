de0f1c35e1b123acf5ebcaf3a4d192fc9596bc97
Maria Luísa Mognato de Medeiros <marialuisamognatodemedeiros@gmail.com>
2025-09-03 11:49:11 -0300

    Feat: Atividade06 - Reajuste no salário

---
+++ Atividade06/Atividade06/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+decimal salario;
+int reajuste;
+
+Console.WriteLine("Digite o valor do seu salário: ");
+salario = decimal.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite a porcentagem de reajuste: ");
+reajuste = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"O valor do seu novo salário é de: R$ {(salario * reajuste / 100) + salario}");
