a6cccc38f5848b69498da4fa79b5d8d33bcd176f
Maria Luísa Mognato de Medeiros <marialuisamognatodemedeiros@gmail.com>
2025-09-03 10:32:27 -0300

    Feat: Atividade02 - Antecessor do número.

---
+++ Atividade02/Atividade02/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int num1;
+Console.WriteLine("Digite um número: ");
+num1 = int.Parse(Console.ReadLine());
+Console.WriteLine($"O antecessor do seu número é: {num1 - 1}");
