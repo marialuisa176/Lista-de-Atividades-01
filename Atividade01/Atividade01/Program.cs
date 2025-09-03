651169ed60d395a83869b9df3bb9c02d5f7aa3ac
Maria Luísa Mognato de Medeiros <marialuisamognatodemedeiros@gmail.com>
2025-09-03 10:28:26 -0300

    Feat: Atividade 1 - Troca de variáveis.

---
+++ Atividade01/Atividade01/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int variavelA = 10;
+int variavelB = 20;
+int variavelAuxiliar = variavelA;
+
+Console.WriteLine($"O valor da variável A agora é: {variavelB} e o valor da variável B agora é: {variavelAuxiliar}");
