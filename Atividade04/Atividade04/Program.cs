20f91b1fc7af3db939ce27349e71d1101fe60f65
Maria Luísa Mognato de Medeiros <marialuisamognatodemedeiros@gmail.com>
2025-09-03 10:50:43 -0300

    Feat: Atividade04 - Idade da pessoa expressa em dias.

---
+++ Atividade04/Atividade04/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int numanos;
+int nummes;
+int numdias;
+
+Console.WriteLine("Informe quantos anos você tem: ");
+numanos = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Informe quantos meses faltam para seu próximo aniversário: ");
+nummes = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Informe quantos dias faltam para seu próximo aniversário (leve em consideração apenas dias, a cada trinta dias deve se considerar um mês): ");
+numdias = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Você tem {numanos * 365 + ((12 - nummes) * 30) + (30 - numdias)} dias");
