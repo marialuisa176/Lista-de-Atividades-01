d425fc6a52008539b917661f371a272a946881b4
Maria Luísa Mognato de Medeiros <marialuisamognatodemedeiros@gmail.com>
2025-09-03 10:38:36 -0300

    Feat: Atividade03 - Área do retângulo.

---
+++ Atividade03/Atividade03/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int numba;
+int numalt;
+
+Console.WriteLine("Digite a base do retângulo: ");
+numba = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite a altura do retângulo: ");
+numalt = int.Parse(Console.ReadLine());
+Console.WriteLine($"A área do retângulo é: {numba * numalt}");
