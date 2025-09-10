24e9408c2ee6a17a78b90b0182129f1ea347ea58
Maria Luísa M. de Medeiros <marialuisamognatodemedeiros@gmail.com>
2025-09-10 07:47:00 -0300

    Feat: Atividade 7 - Informar se o número digitado é maior que 10

---
+++ Atividade07/Atividade07/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int num1;
+
+Console.WriteLine("Digite um número: ");
+num1 = int.Parse(Console.ReadLine());
+if (num1 > 10)
+{
+    System.Console.WriteLine("O número digitado é maior que 10!");
+}
+else
+{
+    System.Console.WriteLine("O número digitado é menor que 10!");
+}
