c52b254417951be116305b290519f251c751c14c
Maria Luísa M. de Medeiros <marialuisamognatodemedeiros@gmail.com>
2025-09-10 09:10:24 -0300

    Feat: Atividade 12 - Descobrir qual é o maior número.

---
+++ Atividade12/Atividade12/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int num1, num2;
+
+Console.WriteLine("Digite um número: ");
+num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite outro número (ele deve ser diferente do primeiro): ");
+num2 = int.Parse(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine("O maior número é o número 1");
+}
+else
+{
+    Console.WriteLine("O maior número é o número 2");
+}
