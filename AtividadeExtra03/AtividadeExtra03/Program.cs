efd1d233c14105a26b37ff42c12c2180d149368c
Maria Luísa M. de Medeiros <marialuisamognatodemedeiros@gmail.com>
2025-09-10 10:53:12 -0300

    Feat: Atividade extra 3 - Cálculo de equações de segundo grau.

---
+++ AtividadeExtra03/AtividadeExtra03/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+decimal numA, numB, numC;
+
+Console.WriteLine("Digite um valor para A: ");
+numA = decimal.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite um valor para B: ");
+numB = decimal.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite um valor para C: ");
+numC = decimal.Parse(Console.ReadLine());
+
+Console.WriteLine($"O valor de delta é: {numB * numB - 4 * numA * numC}");
+
