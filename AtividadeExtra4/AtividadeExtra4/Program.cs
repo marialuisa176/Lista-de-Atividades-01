5ebdf8edf3d2943b11d4918aaa3cabadd92d25e1
Maria Luísa M. de Medeiros <marialuisamognatodemedeiros@gmail.com>
2025-09-10 11:05:44 -0300

    Feat: Atividade extra 4 - Calcular o preço de um produto com desconto.

---
+++ AtividadeExtra4/AtividadeExtra4/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+double valorprod;
+
+Console.WriteLine("Informe o valor do produto: ");
+valorprod = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"O produto com desconto é: {valorprod * 0.95} ");
