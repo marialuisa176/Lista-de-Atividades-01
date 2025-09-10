ca3e6832ab83d14cae34c82ba20523b666b4fae7
Maria Luísa M. de Medeiros <marialuisamognatodemedeiros@gmail.com>
2025-09-10 10:15:45 -0300

    Feat: Atividade Extra 01 - Cálculo de quantos dólares é possível comprar.

---
+++ Atividade13/Atividade13/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int num1, num2, num3;
+
+Console.WriteLine("Digite um número: ");
+num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite outro número (diferente do primeiro): ");
+num2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite outro número (diferente dos dois anteriores: ");
+num3 = int.Parse(Console.ReadLine());
+
+int[] valores = { num1, num2, num3 };
+//O método Array.Sort() serve para ordenar os elementos do array
+//em ordem crescente (do menor para o maior) automaticamente.
+Array.Sort(valores);
+
+Console.WriteLine("Valores em ordem crescente:");
+//O foreach é um laço de repetição (loop) em C# que permite percorrer todos os elementos de uma coleção,
+//como um array, uma lista ou qualquer outro tipo de coleção que implementa a interface
+foreach (int valor in valores)
+{
+    Console.WriteLine(valor);
+}
---
+++ AtividadeExtra01/AtividadeExtra01/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+decimal valoremreal;
+
+Console.WriteLine("Digite quantos reais você tem na cateira: ");
+valoremreal = decimal.Parse(Console.ReadLine());
+
+Console.WriteLine($"O valor que você pode comprar em dólar é: $ {(double)valoremreal / 3.45}");
