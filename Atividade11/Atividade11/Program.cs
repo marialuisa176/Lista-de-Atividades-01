a4bb14fe893f19904cf2b1079834977145c1487c
Maria Luísa M. de Medeiros <marialuisamognatodemedeiros@gmail.com>
2025-09-10 08:53:31 -0300

    Feat: Atividade 11 - Informa se o voto é obrigatório ou não.

---
+++ Atividade11/Atividade11/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int anodenasc, anoatual, idade;
+
+Console.WriteLine("Informe em que ano estamos: ");
+anoatual = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Informe em que ano você nasceu: ");
+anodenasc = int.Parse(Console.ReadLine());
+
+if (anoatual - anodenasc > 18 && anoatual - anodenasc < 60)
+{
+    Console.WriteLine("Você é obrigado a votar esse ano");
+}
+else
+{
+    Console.WriteLine("Você não é obrigado a votar esse ano");
+}
