35dfbc8cdff031660e428e1d6b23c0e031fc8d48
Maria Luísa M. de Medeiros <marialuisamognatodemedeiros@gmail.com>
2025-09-10 08:31:46 -0300

    Feat: Atividade 10 - Média das notas dos alunos.

---
+++ Atividade10/Atividade10/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net.Http.Headers;
+
+double nota1;
+double nota2;
+
+Console.WriteLine("Qual foi sua nota na primeira avaliação? ");
+nota1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Qual foi sua nota na segunda avaliação? ");
+nota2 = double.Parse(Console.ReadLine());
+
+double media = nota1 + nota2 / 2;
+if (media >= 6)
+{
+    Console.WriteLine("Você está APROVADO");
+}
+else
+{
+    Console.WriteLine("Você está REPROVADO");
+}
