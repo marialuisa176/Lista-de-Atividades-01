6e67b4a29e818c3cc4572c78c48ccfbe982010d3
Maria Luísa M. de Medeiros <marialuisamognatodemedeiros@gmail.com>
2025-09-10 08:17:21 -0300

    Feat: Atividade 09 - Valor total das maçãs.

---
+++ Atividade09/Atividade09/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int nummacas;
+
+Console.WriteLine("Quantas maçãs você comprou? ");
+nummacas = int.Parse(Console.ReadLine());
+
+if (nummacas >= 12)
+    {
+    System.Console.WriteLine($"O valor total é: R$ {nummacas * 1}");
+    }
+else
+    {
+    System.Console.WriteLine($"O valor total é: R$ {nummacas * 1.30}");
+    }
