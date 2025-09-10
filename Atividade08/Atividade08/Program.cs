7e4327c3e9dfac2870178e5acde071547ce22fda
Maria Luísa M. de Medeiros <marialuisamognatodemedeiros@gmail.com>
2025-09-10 07:55:46 -0300

    Feat: Atividade 8 - Informar se um número é positivo ou negativo

---
+++ Atividade08/Atividade08/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int num1;
+
+Console.WriteLine("Digite um número (não é permitido 0): ");
+num1 = int.Parse(Console.ReadLine());
+
+if (num1 > 0)
+{
+    Console.WriteLine("O número digitado é positivo");
+}
+else
+{
+    Console.WriteLine("O número digitado é negativo");
+}
