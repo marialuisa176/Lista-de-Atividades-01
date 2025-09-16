b707c0cbd047b6c0d1a3e504fc8c2fba6a08b823
Maria Luísa M. de Medeiros <marialuisamognatodemedeiros@gmail.com>
2025-09-16 08:17:25 -0300

    Feat: Atividade 14 - Ler um valor e informar seus números anteriores (usando for).

---
+++ Atividade14/Atividade14/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int valorN;
+
+Console.WriteLine("Digite um número");
+valorN = int.Parse(Console.ReadLine());
+for (int i = 1; i <= valorN; i++)
+{
+    Console.WriteLine(i);
+}
