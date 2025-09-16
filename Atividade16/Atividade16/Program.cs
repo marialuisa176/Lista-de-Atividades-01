aced3aac59457a551849fb6a1c93891480d8ed90
Maria Luísa M. de Medeiros <marialuisamognatodemedeiros@gmail.com>
2025-09-16 08:55:02 -0300

    Feat: Atividade 16 - Algoritmo para calcular fatorial de um número.

---
+++ Atividade16/Atividade16/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int num;
+int fatorial = 1;
+
+Console.WriteLine("Digite um número");
+num = int.Parse(Console.ReadLine());
+
+// Calculando o fatorial de n
+for (int i = 1; i <= num; i++)
+{
+    fatorial *= i;  // Multiplica o valor de fatorial pelo valor de i
+}
+Console.WriteLine($"O fatorial de {num} é: {fatorial}");
+
