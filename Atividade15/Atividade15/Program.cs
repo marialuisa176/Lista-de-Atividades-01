7e28753e74b25ba0c20ed5deca5e24c1e667774b
Maria Luísa M. de Medeiros <marialuisamognatodemedeiros@gmail.com>
2025-09-16 08:36:32 -0300

    Feat: Atividade 15 - Informar os números que quando divididos por 11 tem resto igual a 5.

---
+++ Atividade15/Atividade15/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int num;
+for (int i = 1000; i <= 2000; i++)
+{
+    if (i % 11 == 5)
+    {
+        Console.WriteLine(i);
+    }
+}
